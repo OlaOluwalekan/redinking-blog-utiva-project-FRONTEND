af7105ed5f1520ee9207ec1428096699fc9334c0
OlaOluwalekan <olalekanbello534@gmail.com>
2023-07-09 15:31:12 +0100

    completed hero section including dark mode

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
 import styles from '../../css/home.module.css'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   const { darkMode } = useSelector((store) => store.user)
@@ -10,7 +11,26 @@ const Hero = () => {
         src='https://res.cloudinary.com/dyyoorpns/image/upload/v1688895314/RedInking/Static%20Images/Hero/redinking_hero_-_red0_-_MOBILE_uersrw.png'
         alt='hero banner'
       />
-      <div></div>
+      <section>
+        <p>
+          Have you <span className={styles.strike}>read</span>{' '}
+          <span className={styles.red}>red</span> this{' '}
+          <span className={styles.ink}>ink</span>?
+        </p>
+
+        <h3>
+          From programming to crypto-currencies; from cooking recipe to health
+          talk;{' '}
+          <span>
+            Here is a blogging platform you will find anything you ever needed
+          </span>
+        </h3>
+
+        <article>
+          <Link to='/auth/login'>Login</Link>
+          <Link to='/auth/register'>Get Started</Link>
+        </article>
+      </section>
     </div>
   )
 }
