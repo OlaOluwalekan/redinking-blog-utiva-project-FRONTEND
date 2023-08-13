e18f7544ab9c0dffbf4e5f2eb16761d34e77afaf
OlaOluwalekan <olalekanbello534@gmail.com>
2023-08-13 19:49:21 +0100

    connected to render instead

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const customFetch = axios.create({
-  baseURL: 'https://redinking.cyclic.cloud/api/v1',
+  // baseURL: 'https://redinking.cyclic.cloud/api/v1',
+  baseURL: 'https://redinking.onrender.com/api/v1',
   // baseURL: 'http://localhost:9000/api/v1',
 })
 
